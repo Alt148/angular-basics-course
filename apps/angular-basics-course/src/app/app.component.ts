6b37f8680b9eed2ffe592df2e443a8e1adae5e28
Jorgen de Groot <degroot@comandi.nl>
2021-12-03 13:49:15 +0100

    Changed prefix to abbreviation of project name

--- apps/angular-basics-course/src/app/app.component.ts
+++ apps/angular-basics-course/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'angular-basics-course-root',
+  selector: 'abs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
